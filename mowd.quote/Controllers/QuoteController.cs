33e6e799d2026a8d191e06695e2856b64445e0d6
Shamus Schubert <shamusschubert@gmail.com>
2018-11-28 12:26:30 -0500

    asd

---
+++ mowd.quote/Controllers/QuoteController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using mowd.quote.Models;
+
+namespace mowd.quote.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuoteController : ControllerBase
+    {
+        [HttpPost()]
+        public async Task<IActionResult> Request([FromBody]RequestModel Model)
+        {
+            if(ModelState.IsValid)
+            {
+                return Created("/api/quote/" + Model.Id, Model);
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ mowd.quote/Models/RequestModel.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mowd.quote.Models
+{
+    public class RequestModel
+    {
+        [JsonIgnore()]
+        public Guid Id { get; private set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("postalCode")]
+        public string PostalCode { get; set; }
+
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+
+        [JsonProperty("mow")]
+        public bool Mow { get; set; }
+
+        [JsonProperty("edge")]
+        public bool Edge { get; set; }
+
+        [JsonProperty("blow")]
+        public bool Blow { get; set; }
+
+        [JsonProperty("pets")]
+        public bool Pets { get; set; }
+
+        [JsonProperty("needBy")]
+        public DateTime NeedBy { get; set; }
+
+        [JsonProperty("noEarlierThan")]
+        [Range(8, 20, ErrorMessage = "No earlier than hour cannot be earlier than 8am local time")]
+        public int NoEarlierThan { get; set; }
+
+        [JsonProperty("noLaterThan")]
+        [Range(8, 20, ErrorMessage = "No later than hour cannot be later than 8pm local time")]
+        public int NoLaterThan { get; set; }
+
+        [JsonProperty("specialInstructions")]
+        public string SpecialInstructions { get; set; }
+    }
+}
