3accd10770435d9eb8fe2de3c99573f512d34b0b
Shamus Schubert <shamusschubert@gmail.com>
2018-12-04 16:45:57 -0500

    done for now

---
+++ mowd.quote.database/Requests.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Requests]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT newid(), 
+    [Address] NVARCHAR(255) NOT NULL, 
+    [City] NVARCHAR(255) NOT NULL, 
+    [State] NVARCHAR(255) NOT NULL, 
+    [PostalCode] NVARCHAR(50) NOT NULL, 
+    [Phone] NVARCHAR(50) NOT NULL, 
+    [Mow] BIT NOT NULL, 
+    [Edge] BIT NOT NULL, 
+    [Blow] BIT NOT NULL, 
+    [Pets] BIT NOT NULL, 
+    [SpecialInstructions] NVARCHAR(255) NULL, 
+    [NeedBy] DATE NULL, 
+    [NoEarlierThan] TIME NULL, 
+    [NoLaterThan] TIME NULL, 
+    [FK_CreatedByUserId] UNIQUEIDENTIFIER NOT NULL
+)
